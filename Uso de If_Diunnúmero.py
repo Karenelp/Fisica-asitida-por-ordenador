1c3ffa689269e078ee2056f8abec4ca73128d88f
Karenelp <karen.parrayepes@gmail.com>
2023-09-22 16:43:20 +0200

    Initial commit

---
+++ Uso de If_Diunnúmero.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 13:24:21 2023
+
+@author: kaths
+"""
+
+n = int(input("Dime un número entero:"))
+if n%2==0:
+    print(n,"es un número par")
+    if n==6:
+        print("Qué poco creativo")
+    else:
+        print("pero podías pensar en algo más original")
+else :
+    print (n,"es un número impar")
+    print ("y por tanto, se va a morir tu gato")
